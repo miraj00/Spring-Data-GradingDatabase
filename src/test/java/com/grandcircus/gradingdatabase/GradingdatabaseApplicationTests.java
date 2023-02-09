3ac226eb048b8147e278c311f083eec8e8805528
miraj <miraj00@gmail.com>
2023-02-09 17:34:05 -0500

    initial code

---
+++ src/test/java/com/grandcircus/gradingdatabase/GradingdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandcircus.gradingdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
